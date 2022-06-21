c0324f45d3f104cd6a1a2ab20b7c63beca97926c
Masterundead01 <serandrus@hotmail.com>
2022-06-21 06:29:13 -0500

    Agregando AudioManager
    - Todas las escenas cuentan con audio manager para reproducir pistas de audio

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,9 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    private void Start()
+    {
+        FindObjectOfType<AudioManager>().Play("Efectos_Sonido_4");
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Seleccion_Personaje");
+        FindObjectOfType<AudioManager>().Pause("Efectos_Sonido_4");
     }
 
     public void QuitGame()
